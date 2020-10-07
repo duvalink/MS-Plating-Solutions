b87281549c87d1a35bd5361d4881fc4e150ed109
duvalink <jose86mend@gmail.com>
2020-10-06 20:37:16 -0700

    se configuro seccion de contacto

--- contactform.php
+++ contactform.php
@@ -1,20 +1,15 @@
 <?php
+$destinatario = 'jose86mend@gmail.com';
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $lastName = $_POST['lastName'];
-    $mailFrom = $_POST['mail'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+$nombre = $_POST['name'];
+$apellido = $_POST['lastname'];
+$mail = $_POST['mail'];
+$telefono = $_POST['phone'];
+$mensaje = $_POST['message'];
 
-    if (empty($name) || empty($lastName) || empty($mailFrom) || empty($phone) || empty($message)) {
-        header('Location::index.php?error');
-    } else {
-        $to = "jose86mend@gmail.com";
-        if (mail($to, $name, $lastName, $mailFrom, $phone, $message)) {
-            header('Location:index.php?success');
-        }
-    }
-} else {
-    header('Location:index.php');
-}
+$header = 'Enviado desde la pagina web de MS Plating Solutions';
+$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+mail($destinatario, $nombre, $mensajeCompleto, $header);
+echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
