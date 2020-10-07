03924b2bf75a8d4350047e2d71d161d387ee9545
duvalink <jose86mend@gmail.com>
2020-10-06 17:35:46 -0700

    se configuro seccion de contacto

--- php/contactform.php
+++ php/contactform.php
@@ -10,7 +10,7 @@ if (isset($_POST['submit'])) {
     $headers = "From: " . $mailFrom;
     $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
 
-    mail($mailTo, $subject, $txt, $headers);
+    mail($mailTo, $mailTo, $txt, $headers);
     header("Location: index.php?mailsend");
 }
 
