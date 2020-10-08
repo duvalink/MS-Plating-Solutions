a0b42fcbb973cabfdf3edab755a149e0d4aa1d27
duvalink <66855905+duvalink@users.noreply.github.com>
2020-10-08 12:12:40 -0700

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,5 +1,5 @@
 <?php
-$destinatario = 'jose86mend@gmail.com';
+$destinatario = 'ssamaniego@manufacturingsolutions.com.mx';
 
 $nombre = $_POST['name'];
 $apellido = $_POST['lastname'];
@@ -8,8 +8,8 @@ $telefono = $_POST['phone'];
 $mensaje = $_POST['message'];
 
 $header = 'Enviado desde la pagina web de MS Plating Solutions';
-$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre . $apellido;
 
 mail($destinatario, $nombre, $mensajeCompleto, $header);
-echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<script>alert('mail sent successfully')</script>";
 echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
